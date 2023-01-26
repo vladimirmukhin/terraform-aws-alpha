534a5822f1d2453d195d7f9bdc07b8c5bdf97d41
Vladimir <awsvlad@gmail.com>
2023-01-26 14:40:23 -0500

    Add more resources

--- feature.tf
+++ feature.tf
@@ -14,3 +14,37 @@ resource "aws_subnet" "secondary" {
 locals {
   secret_password = var.secret_password
 }
+
+resource "aws_subnet" "private" {
+  count = length(var.public_cidr)
+
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.private_cidr[count.index]
+
+  availability_zone = var.availability_zones[count.index]
+
+  tags = {
+    Name = "${var.env_code}-private${count.index+1}"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = var.env_code
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "${var.env_code}-public"
+  }
+}
