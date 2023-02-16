2d2cb5660965861d5ae64a44531f89c8ae420a0c
Vladimir <awsvlad@gmail.com>
2023-02-16 14:36:21 -0500

    Add lock table

--- provider.tf
+++ provider.tf
@@ -1,7 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-remote-state-abc123abc"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
+    bucket         = "terraform-remote-state-abc123abc"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-remote-state-lock"
   }
 }
