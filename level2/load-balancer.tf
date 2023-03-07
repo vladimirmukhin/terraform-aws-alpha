b96470a790839ec081ffe251713493889ad64244
Vladimir <awsvlad@gmail.com>
2023-03-07 14:50:55 -0500

    Add acm certificate

--- level2/load-balancer.tf
+++ level2/load-balancer.tf
@@ -1,7 +1,8 @@
 module "load_balancer" {
   source = "../modules/lb"
 
-  vpc_id     = data.terraform_remote_state.level1.outputs.vpc_id
-  env_code   = var.env_code
-  subnet_ids = data.terraform_remote_state.level1.outputs.public_subnet_ids
+  vpc_id          = data.terraform_remote_state.level1.outputs.vpc_id
+  env_code        = var.env_code
+  subnet_ids      = data.terraform_remote_state.level1.outputs.public_subnet_ids
+  certificate_arn = aws_acm_certificate.main.arn 
 }
--- modules/lb/load-balancer.tf
+++ modules/lb/load-balancer.tf
@@ -5,8 +5,8 @@ resource "aws_security_group" "load_balancer" {
 
   ingress {
     description = "http to ELB"
-    from_port   = 80
-    to_port     = 80
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -55,8 +55,10 @@ resource "aws_lb_target_group" "main" {
 
 resource "aws_lb_listener" "main" {
   load_balancer_arn = aws_lb.main.arn
-  port              = "80"
-  protocol          = "HTTP"
+  port              = "443"
+  protocol          = "HTTPS"
+  certificate_arn   = var.certificate_arn 
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
 
   default_action {
     type             = "forward"
