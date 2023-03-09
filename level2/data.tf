013461c64245c11f319696c5068719ba87397a4f
Vladimir <awsvlad@gmail.com>
2023-03-09 14:50:26 -0500

    Configure RDS

--- level2/data.tf
+++ level2/data.tf
@@ -7,3 +7,11 @@ data "terraform_remote_state" "level1" {
     region = "us-east-1"
   }
 }
+
+data "aws_secretsmanager_secret" "rds_password" {
+  name = "rds/password"
+}
+
+data "aws_secretsmanager_secret_version" "rds_password" {
+  secret_id = data.aws_secretsmanager_secret.rds_password.id
+}
---
+++ modules/rds/variables.tf
@@ -0,0 +1,13 @@
+variable "env_code" {}
+
+variable "subnet_ids" {}
+
+variable "vpc_id" {}
+
+variable "db_name" {}
+
+variable "db_user" {}
+
+variable "db_password" {}
+
+variable "source_security_group" {}
